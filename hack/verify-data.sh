e60156adb584fa158d1d70e0338d8a9cb986a4b7
Simon Baird <sbaird@redhat.com>
2025-02-12 11:54:58 -0500

    Validate data using both latest & konflux bundles
    Checking against the "konflux" tag could prevent an immediate
    breakage. Checking againstly the "latest" tag could prevent a
    breakage that would happen the next time the :konflux tag gets
    updated, which is currently every Tuesday, see [1].
    
    It's pretty easy to check both to avoid both immediate and future
    breakages, so let's do that.
    
    [1] https://github.com/enterprise-contract/infra-deployments-ci/blob/main/.github/workflows/konflux-policy.yaml

--- hack/verify-data.sh
+++ hack/verify-data.sh
@@ -16,23 +16,30 @@ if [[ -n "${outdated}" && "${outdated}" != "[]" ]]; then
 fi
 echo '✅ data/known_rpm_repositories.yml has expected extras'
 
-# The EC policy does not allow for relative paths. For this reason, we use envsubst to replace
-# occurrences of $PWD with the actual working directory. The result is a temporary policy file
-# with absolute paths.
-# NOTE: An alternative to saving the modified policy to a temporary file is to use a heredoc, e.g.
-# `ec validate input --file <(...)` However, when doing so, the file name is something like
-# `/dev/fd/63` which EC does not understand as neither a JSON nor a YAML file. Instead EC tries to
-# fetch such resource from a Kubernetes cluster 😅
-POLICY_YAML="$(mktemp --suffix '.yaml')"
-< policy.yaml envsubst '$PWD' > "${POLICY_YAML}"
+# The konflux tag is what is used in Konflux prod. It is updated weekly. For reference:
+# https://github.com/redhat-appstudio/infra-deployments/blob/main/components/enterprise-contract/ecp.yaml
+# https://github.com/enterprise-contract/infra-deployments-ci/blob/main/.github/workflows/konflux-policy.yaml
+# Check against them both to potentially catch an immediate breakage and a future breakage.
+for tag in konflux latest; do
 
-ec validate policy --policy "${POLICY_YAML}"
-echo '✅ Policy config validated'
+  # The EC policy does not allow for relative paths. For this reason, we use envsubst to replace
+  # occurrences of $PWD with the actual working directory. The result is a temporary policy file
+  # with absolute paths.
+  # NOTE: An alternative to saving the modified policy to a temporary file is to use a heredoc, e.g.
+  # `ec validate input --file <(...)` However, when doing so, the file name is something like
+  # `/dev/fd/63` which EC does not understand as neither a JSON nor a YAML file. Instead EC tries to
+  # fetch such resource from a Kubernetes cluster 😅
+  POLICY_YAML="$(mktemp --suffix '.yaml')"
+  < policy.yaml env POLICY_TAG=$tag envsubst '$PWD,$POLICY_TAG' > "${POLICY_YAML}"
 
-# The command requires --file to be used at least once. This sets the input to be verified by the
-# policy rules. However, here we are verifying the data sources which does not require an input.
-# So we use a dummy input file instead.
-ec validate input --policy "${POLICY_YAML}" --output yaml --file <(echo '{}') | yq .
-echo '✅ Data validated'
+  ec validate policy --policy "${POLICY_YAML}"
+  echo "✅ Policy config validated ($tag tag)"
 
-rm -f "${POLICY_YAML}"
+  # The command requires --file to be used at least once. This sets the input to be verified by the
+  # policy rules. However, here we are verifying the data sources which does not require an input.
+  # So we use a dummy input file instead.
+  ec validate input --policy "${POLICY_YAML}" --output yaml --file <(echo '{}') | yq .
+  echo "✅ Data validated ($tag tag)"
+
+  rm -f "${POLICY_YAML}"
+done
